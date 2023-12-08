95bb351d729b69de874888ff047df534b2201c55
aquinteror <Aquinteror@intergrupo.com>
2023-12-07 19:07:10 -0500

    implementación front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +26,18 @@ import { CarComponent } from './car/car.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NzTableModule,
+    NzButtonModule,
+    NzSelectModule,
+    NzFormModule,
+    NzGridModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ICalculateTotalRequest, ICalculateTotalResponse } from 'src/app/models/car.model'
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-car',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+  public form: FormGroup;
+  public prueba: number | null = 0;
 
-  constructor() { }
+  requestModel: ICalculateTotalRequest = {
+    basePrice: 0,
+    vehicleType: ''
+  };
+
+  responseModel: ICalculateTotalResponse[] = [];
+
+
+  constructor(private priceCalculationService: ApiService, private formbuilder: FormBuilder) {
+    this.form = this.formbuilder.group(
+      {
+        basePrice: ['', { validators: [Validators.required] }],
+        vehicleType: ['', { validators: [Validators.required] }]
+      })
+  }
 
   ngOnInit(): void {
+  }
+
+  calculateTotal() {
+    const request: ICalculateTotalRequest = {
+      basePrice: this.form.value.basePrice,
+      vehicleType: this.form.value.vehicleType
+    };
+
+    console.log(request);
+
+    this.priceCalculationService.calculateTotal(request).subscribe(
+      (result: ICalculateTotalResponse) => {
+        this.responseModel.push(result);
+        console.log(this.responseModel)
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+
   }
 
 }
